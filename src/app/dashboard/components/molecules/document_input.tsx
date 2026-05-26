ea59148a0b5e4ea47f9e44e015e677ca70d6f881
lucas.nobrega@afinz.com.br <lucasnobrega.js@gmail.com>
2026-05-26 19:34:53 -0300

    Trocando por label

--- src/app/dashboard/components/molecules/document_input.tsx
+++ src/app/dashboard/components/molecules/document_input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useCallback } from "react";
+import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import { BsCloudUpload } from "react-icons/bs";
 import { HiDocumentCheck } from "react-icons/hi2";
@@ -12,18 +12,23 @@ interface Props {
 }
 
 export function DashboardDocumentInput(props: Props) {
-  const onDrop = useCallback(
-    (file?: File) => {
-      props.onInput(file);
-      console.log("Arquivos aceitos:", file);
+  const onDropAccepted = useCallback(
+    (acceptedFiles: File[]) => {
+      console.log("onDropAccepted", acceptedFiles);
+      if (acceptedFiles.length > 0) {
+        props.onInput(acceptedFiles[0]);
+      }
     },
     [props],
   );
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    onDrop: (files) => {
-      console.log("Arquivo selecionado.");
-      onDrop(files.length > 0 ? files[0] : undefined);
+  const { getRootProps, getInputProps } = useDropzone({
+    onDropAccepted,
+    onDropRejected: (rejectedFiles) => {
+      console.log("onDropRejected", rejectedFiles);
+    },
+    onFileDialogCancel: () => {
+      console.log("onFileDialogCancel");
     },
     accept: {
       "application/pdf": [".pdf"],
@@ -33,68 +38,46 @@ export function DashboardDocumentInput(props: Props) {
     multiple: false,
   });
 
-  const renderDraggbleContent = useCallback(() => {
-    return (
-      <div
-        className="
-          flex
-          flex-col
-          items-center
-          gap-0!
-          justify-center
-          text-center
-        "
-      >
-        <BsCloudUpload size={40} />
+  const inputProps = getInputProps({
+    onChange(event) {
+      console.log("input onChange", event.target.files);
+    },
+  });
 
+  const renderDraggbleContent = useCallback(
+    () => (
+      <div className="flex flex-col items-center gap-0! justify-center text-center">
+        <BsCloudUpload size={40} />
         <h4 className="mt-2">Clique para selecionar ou arraste o PDF</h4>
         <h5 className="text-sm opacity-60">PDF ou DOCX (Máx. 10MB)</h5>
       </div>
-    );
-  }, []);
+    ),
+    [],
+  );
 
-  const renderFulledDraggble = useCallback(() => {
-    return (
-      <div
-        className="
-          flex
-          flex-col
-          items-center
-          gap-0!
-          justify-center
-          text-center
-        "
-      >
+  const renderFulledDraggble = useCallback(
+    () => (
+      <div className="flex flex-col items-center gap-0! justify-center text-center">
         <HiDocumentCheck size={40} />
-
-        <h4 className="mt-2">{props.file!.name}</h4>
+        <h4 className="mt-2">{props.file?.name}</h4>
       </div>
-    );
-  }, [props.file]);
+    ),
+    [props.file],
+  );
 
   return (
-    <label
+    <div
       {...getRootProps()}
       className={`
-        border
-        border-dashed
+        border border-dashed
         ${props.file ? "border-green-400! bg-green-50 text-green-900!" : ""}
-
-        p-3
-        h-60
-        rounded-2xl
-
-        flex
-        flex-col
-        items-center
-        justify-center
-
+        p-3 h-60 rounded-2xl
+        flex flex-col items-center justify-center
         cursor-pointer
       `}
     >
-      <input {...getInputProps()} className="hidden" name={props.name} />
-
+      <input {...inputProps} className="hidden" name={props.name} />
       {props.file ? renderFulledDraggble() : renderDraggbleContent()}
-    </label>
+    </div>
   );
 }
